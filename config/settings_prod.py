bc5892bde83c163b3428cba5e91ede519149e435
sameer gul <sameergul12345@gmail.com>
2025-02-03 03:14:55 +0500

    Add debug logging for database connection

--- config/settings_prod.py
+++ config/settings_prod.py
@@ -56,9 +56,8 @@ DATABASES = {
         'PORT': os.getenv('DB_PORT', '3306'),
         'OPTIONS': {
             'charset': 'utf8mb4',
-            'connect_timeout': 30,
+            'connect_timeout': 60,
             'autocommit': True,
-            'isolation_level': 'read committed',
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
         }
     }
@@ -80,7 +79,7 @@ EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
 
-# Logging
+# Enhanced logging for database issues
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
@@ -96,8 +95,8 @@ LOGGING = {
     'loggers': {
         'django.db.backends': {
             'handlers': ['console'],
-            'level': 'ERROR',
-            'propagate': False,
+            'level': 'DEBUG',
+            'propagate': True,
         },
     },
 }
