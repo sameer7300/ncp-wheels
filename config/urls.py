28f7f7afc0eba39dc48b6fd11e238fd063c447cb
sameer gul <sameergul12345@gmail.com>
2025-02-03 14:14:37 +0500

    Remove debug_toolbar URL patterns

--- config/urls.py
+++ config/urls.py
@@ -22,8 +22,3 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    
-    import debug_toolbar
-    urlpatterns += [
-        path('__debug__/', include(debug_toolbar.urls)),
-    ]
